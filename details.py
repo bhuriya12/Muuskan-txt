09a84d505f06866b7ecac40a7dd68f30670cafbe
bhuriya12 <162349127+bhuriya12@users.noreply.github.com>
2024-08-06 22:16:08 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,3 +1,3 @@
 api_id = 26451206
 api_hash = "32984406271d6f3945bb536671b143a7"
-bot_token = "7488912906:AAGPn1KPQUyQ3PryQg0hW5I8QfmgQ5mDGhE"
+bot_token = "7333551716:AAE4fmoxyNby_fT5gPuPrM3s-wd1Uh-x0uk"
